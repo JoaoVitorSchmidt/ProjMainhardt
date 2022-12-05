17e643fb1dfdafdcadb714a5575c379bb5ba0550
JoaoVitorSchmidt <67595544+JoaoVitorSchmidt@users.noreply.github.com>
2022-12-05 16:35:06 -0300

    4 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import './operations.js';
 
 function App() {
   return (
@@ -9,37 +8,85 @@ function App() {
       <h1>Formulário</h1>
 
       <form action="" method="get">
-        <label for="GetNome" class="nome">
+        <label for="Nome" class="nome">
           Nome Completo:
           <input type="text" name="nome" />
         </label>
         <br />
-        <label for="GetEmail" class="email">
+        <label for="Email" class="email">
           E-mail:
           <input type="text" name="email" class="texto" />
         </label>
         <br />
-        <label for="GetDataEntrega" class="data">
+        <label for="DataEntrega" class="data">
           Data de Entrega:
           <input type="text" name="dataEntrega" />
         </label>
         <br />
-        <label for="GetCEP" class="cep">
+        <label for="CEP" class="cep" id="CEP">
           CEP:
-          <input type="text" name="cep" class="texto" />
+          <input
+            type="text"
+            name="cep"
+            class="PosCEP"
+            onChange = "function (verificaCep());"
+          />
         </label>
         <br />
-        <label for="GetEndereco">
+        <label for="Logradouro">
+          Logradouro:
+          <input type="text" name="logradouro" class="PosCEP" id="logradouro"/>
+        </label>
+        <br />
+        <label for="Bairro">
+          Bairro:
+          <input type="text" name="bairro" class="PosCEP" />
+        </label>
+        <br />
+        <label for="Localidade">
+          Localidade:
+          <input type="text" name="localidade" class="PosCEP" />
+        </label>
+        <br />
+        <label for="UF">
+          UF:
+          <input type="text" name="uf" class="PosCEP" />
+        </label>
+        <br />
+        <label for="IBGE">
+          IBGE:
+          <input type="text" name="ibge" class="PosCEP" />
+        </label>
+        <br />
+        <label for="GIA">
+          GIA:
+          <input type="text" name="gia" class="PosCEP" />
+        </label>
+        <br />
+        <label for="DDD">
+          DDD:
+          <input type="text" name="ddd" class="PosCEP" />
+        </label>
+        <br />
+        <label for="SIAFI">
+          SIAFI:
+          <input type="text" name="siafi" class="PosCEP" />
+        </label>
+        <br />
+        <label for="Endereco">
           Número do Endereço:
           <input type="text" name="endereco" class="texto" />
         </label>
         <br />
-        <label for="GetComplemento">
+        <label for="Complemento">
           Complemento:
           <input type="text" name="complemento" class="texto" />
         </label>
         <br />
-        <button class="botao">Limpar</button>
+        <button class="botao" type="reset">
+          Limpar
+        </button>
+        <br />
       </form>
     </div>
   );
--- src/operations.js
+++ src/operations.js
@@ -1,7 +1,24 @@
-function verificaCep(cep) {
-  if (cep == '01001-000') {
-    
+var json =
+  '{"cep": "01001-000", "logradouro": "Praça da Sé", "complemento": "lado ímpar", "bairro": "Sé", "localidade":"São Paulo", "uf": "SP", "ibge": "3550308", "gia": "1004", "ddd": "11", "siafi": "7107" }';
+
+var obj = JSON.parse(json);
+
+function verificaCep(e) {
+  e.preventDefault();
+
+  const cep = document.getElementById('CEP');
+
+  var log = (document.getElementById('logradouro').readOnly = true);
+
+  if (cep.value == '01001-000') {
+    document.getElementsById('Logradouro').value = obj.logradouro;
+    document.getElementsById('Complemento').value = obj.complemento;
+    document.getElementsById('Bairro').value = obj.bairro;
+    document.getElementsById('Localidade').value = obj.localidade;
+    document.getElementsById('UF').value = obj.uf;
+    document.getElementsById('IBGE').value = obj.ibge;
+    document.getElementsById('GIA').value = obj.gia;
+    document.getElementsById('DDD').value = obj.ddd;
+    document.getElementsById('SIAFI').value = obj.siafi;
   }
 }
-
-function limpa() {}
